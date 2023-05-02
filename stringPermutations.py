4b83b7d6314021f029ce6db9ba6d623918759624
Shubham Mohanty <111239624+shubudubu@users.noreply.github.com>
2023-05-02 12:16:56 +0530

    Update stringPermutations.py

--- stringPermutations.py
+++ stringPermutations.py
@@ -1,15 +1,22 @@
 y=[['A','B','C','D']]
 k=-1
-x=0
+x=-1
+z=0
+print(y[0])
 while True:
     k+=1
     for i in range(0,4):
         for j in range(i+1,4):
             x+=1
-            if x>23:
+            if x>22:
+                z=1
                 break
-            y[k][i],y[k][j]=y[k][j],y[k][i]
+            print(y[k])
             y.append(y[k])
+            y[x+1][i],y[x+1][j]=y[x][j],y[x][i]
+            print(y[x])
+        if z==1:
+            break
+    if z==1:
+        break
            
-for i in range(0,24):
-    print(y[i])
